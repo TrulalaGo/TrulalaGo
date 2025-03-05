cee3b456d5a8c980a8348c20a54cf4cab9b47405
TrulalaGo <trobeltrulala87@gmail.com>
2025-03-06 00:16:06 +0700

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "gas.trulala.trulalago"
+    compileSdk = 33
+    
+    defaultConfig {
+        applicationId = "gas.trulala.trulalago"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        
+    }
+    
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "11"
+}
+
+dependencies {
+
+
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.activity:activity:1.7.2")
+    implementation("com.github.topjohnwu.libsu:core:3.1.2")
+}
